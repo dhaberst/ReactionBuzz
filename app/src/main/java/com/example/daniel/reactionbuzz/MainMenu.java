7cc849f8eee16eadfe0ee292a73b8292ed07c6db
dhaberst <daniel.haberstock1@gmail.com>
2015-09-12 19:08:28 -0600

    Added "Reaction Stats" and "Buzzer Stats" to drop down menu.

--- app/src/main/java/com/example/daniel/reactionbuzz/MainMenu.java
+++ app/src/main/java/com/example/daniel/reactionbuzz/MainMenu.java
@@ -1,9 +1,26 @@
+/*  Reaction Buzz: A few games featuring a reaction timer and a game show buzzer
+    Copyright (C) 2015  Daniel Haberstock dhaberst@ualberta.ca
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.example.daniel.reactionbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -18,6 +35,11 @@ public class MainMenu extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_menu, menu);
         return true;
+    }
+
+    public  void reactionstats(MenuItem menu) {
+        Toast.makeText(this,"Reaction Stats",Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
