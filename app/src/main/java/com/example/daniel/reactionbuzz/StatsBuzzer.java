b93925db7dfe5903a0a60dad529574f4e34175f9
dhaberst <daniel.haberstock1@gmail.com>
2015-09-29 20:48:16 -0600

    Implemented BuzzerStatsActivity.

--- app/src/main/java/com/example/daniel/reactionbuzz/StatsBuzzer.java
+++ app/src/main/java/com/example/daniel/reactionbuzz/StatsBuzzer.java
@@ -1,15 +1,39 @@
 package com.example.daniel.reactionbuzz;
 
+import java.util.ArrayList;
+
 /**
  * Created by Daniel on 2015-09-27.
  */
 public class StatsBuzzer {
-    int players;
 
-    public StatsBuzzer(int players) {
-        this.players = players;
+    protected Integer twoplayerbuzzer[] = {0, 0};
+    protected Integer threeplayerbuzzer[] = {0, 0, 0};
+    protected Integer fourplayerbuzzer[] = {0, 0, 0, 0};
+
+    public void setPlayerCount(int players, int player) {
+        if (players == 2) {
+            twoplayerbuzzer[player] += 1;
+        }
+
+        else if (players == 3) {
+            threeplayerbuzzer[player] += 1;
+        }
+
+        else if (players == 4) {
+            fourplayerbuzzer[player] += 1;
+        }
     }
 
+    public Integer[] getTwoplayerbuzzer() {
+        return twoplayerbuzzer;
+    }
 
+    public Integer[] getThreeplayerbuzzer() {
+        return threeplayerbuzzer;
+    }
 
+    public Integer[] getFourplayerbuzzer() {
+        return fourplayerbuzzer;
+    }
 }
